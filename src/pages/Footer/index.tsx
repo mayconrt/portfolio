226e28de4e71ab8684efaf16aa82b8ff50ec64a3
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2020-08-27 12:43:31 -0300

    finish section footer

--- src/pages/Footer/index.tsx
+++ src/pages/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { FaInstagram, FaFacebookF, FaLinkedin, FaMailBulk } from 'react-icons/fa'
 
 import './style.css'
 
@@ -8,11 +9,32 @@ export default function Header() {
         <section className="ma-section-footer">
             <div className="ma-footer">
                 <div className="footer-social">
-                    <h5>Follow Us On</h5>	
-                    <a href="#" target="_blank" className="fa fa-facebook" title="facebook">
-                    <i className="fa fa-instagram"></i>
-                    </a>			
-                 </div>   
+                    <div className="ma-menu-footer-main">
+                        <div className="ma-menu-footer">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Skills</a></li>
+                                <li><a href="#">Projects</a></li>
+                                <li><a href="#">Contacts</a></li>
+                                <li><a href="#">Clients</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="ma-text-follw"><h5>Follow Us On</h5></div>
+                    <div className="ma-socials">
+                        <div className="ma-socials-icon">
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/maycon.albuquerque.39/" title="Facebook">< FaFacebookF size="30" /></a>
+                        </div>
+                        <div className="ma-socials-icon">
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/maykinhomonstro/?hl=pt-br" title="Instagram">< FaInstagram size="30" /></a>
+                        </div>
+                        <div className="ma-socials-icon">
+                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/maycon-albuquerque-a55010a2" title="Linkedin">< FaLinkedin size="30" /></a>
+                        </div>
+                    </div>
+                    <div className="ma-text-copy"><p>2020 M.R.T.A. All Rights Reserved.</p></div>
+                </div>
             </div>
         </section>
     )
